cfe21d1f5cae318437b7d069e60408c6224d0682
Mohammad-Asraful <asrafmail01@gmail.com>
2023-01-14 00:20:27 +0600

    add lines

--- Javascript/JS-26-M-33.5-API/practice-33.5/randomUser.js
+++ Javascript/JS-26-M-33.5-API/practice-33.5/randomUser.js
@@ -21,10 +21,12 @@ const showData = (details) => {
       dataDiv.style.fontSize = '30px'
       dataDiv.style.border = '3px solid orange'
       dataDiv.style.padding = '20px'
-      dataDiv.style
+      dataDiv.style.borderRadius = '10px'
+      dataDiv.style.width = '650px'
+      dataDiv.style.margin = '0 auto'
 
       dataDiv.innerHTML = `
-      <img src='${datas.picture.large}'  style='width: 250px; display: block; margin-bottom: 30px; '>
+      <img src='${datas.picture.large}'  style='width: 250px; display: block; margin: 0 auto 30px auto;'>
       Location:
          <p>Street Number: ${datas.location.street.number}</p>
          <p>Street Name: ${datas.location.street.name}</p>
@@ -34,8 +36,8 @@ const showData = (details) => {
          <p>Post Code: ${datas.location.postcode}</p>
          <p>Coordinates Latitude: ${datas.location.coordinates.latitude}</p>
          <p>Coordinates Longitude: ${datas.location.coordinates.longitude}</p>
-         <p>Timezone: ${datas.location.timezone.offset}</p>
-         <p>Timezone: ${datas.location.timezone.description}</p>
+         <p>Timezone Offset: ${datas.location.timezone.offset}</p>
+         <p>Timezone Description: ${datas.location.timezone.description}</p>
       `
 
       randomData.appendChild(dataDiv)
